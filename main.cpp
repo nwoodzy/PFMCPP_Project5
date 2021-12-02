8ba67c228df204b41ea2b320c2498f1dedd4c6c6
matkatmusic <matkatmusic@gmail.com>
2021-12-02 13:51:55 -0800

    added experienced instructions

--- main.cpp
+++ main.cpp
@@ -1,79 +1,81 @@
 /*
 Project 5: Part 1 / 4
  video Chapter 2 - Part 12
-
  Create a branch named Part1
-
 Purpose:  This project continues developing Project3.
        you will learn how to take code from existing projects and migrate only what you need to new projects
        you will learn how to write code that doesn't leak as well as how to refactor. 
-
- Destructors
+=============================================
+Since you didn't do Project 3:
+=============================================
+write 3 UDTs below that EACH have: 
+        5 member variables
+            the member variable names and types should be relevant to the work the UDT will perform.
+            pick properties that can be represented with 'int float double bool char std::string'
+        3 member functions with an arbitrary number of parameters
+            give some of those parameters default values.
+        constructors that initialize some of these member variables
+            the remaining member variables should be initialized in-class
+        for() or while() loops that modify member variables
+ 1) 2 of your 3 UDTs need to have a nested UDT.
+    this nested UDT should fulfill the same requirements as above:
+        5 member variables  
+        3 member functions
+        constructors and loops.
         
- 
- 
- 1) Copy 3 of your user-defined types (the ones with constructors and for()/while() loops from Project 3) here
-    Choose the classes that contained nested classes.  Include the nested classes when you copy them over.
-
- 2) move all of your implementations of all functions to OUTSIDE of the class.
-
- 3) add destructors
+ 2) Define your implementations of all functions OUTSIDE of the class. 
+ NO IN-CLASS IMPLEMENTATION ALLOWED
+ 3) add destructors to all of your UDTs
         make the destructors do something like print out the name of the class.
-
+ 
  4) add 2 new UDTs that use only the types you copied above as member variables.
-
- 5) add 2 member functions that use your member variables to each of these new UDTs
-
- 6) Add constructors and destructors to these 2 new types that do stuff.  
+ 
+ 5) Add destructors to these 2 new types that do something.  
         maybe print out the name of the class being destructed, or call a member function of one of the members.  be creative
  
- 7) copy over your main() from the end of Project3 
-        get it to compile with the types you copied over.
-        remove any code in main() that uses types you didn't copy over.
+ 6) add 2 member functions to each of these 2 new types.
+       These member functions should make use of the member variables. 
+       Maybe interact with their properties or call their member functions.
+       The purpose is to demonstrate that you know how to use a class's member variables and member functions when it is a member of another class.
  
- 8) Instantiate your 2 UDT's from step 4) in the main() function at the bottom.
-       call their member functions.
+ 7) use at least 2 instances of each of your UDTs in main. 
+       - call every member function of your UDTs to make sure they work as expected and don't produce warnings.
+       - add some std::cout statements in main() that use your UDT's member variables.
+       you have 5 UDTs and 2 nested UDTs, so there should be at minimum 14 UDTs declared in main(), as well as 14 * 3 function calls happening.
  
- 9) After you finish, click the [run] button.  Clear up any errors or warnings as best you can.
-
- you can resolve any [-Wdeprecated] warnings by adding -Wno-deprecated to list of compiler arguments in the .replit file. all of the "-Wno" in that file are compiler arguments.
- You can resolve any [-Wpadded] warnings by adding -Wno-padded to the list of compiler arguments in the .replit file. all of the "-Wno" in that file are compiler arguments.
-
+ 8) After you finish, click the [run] button.  Clear up any errors or warnings as best you can.
+ 
+If you need inspiration for what to write, take a look at previously approved student projects in the Slack Workspace channel for this project part.
  */
 
 /*
- copied UDT 1:
+ UDT 1:
  */
 
 /*
- copied UDT 2:
+ UDT 2:
  */
 
 /*
- copied UDT 3:
+ UDT 3:
  */
 
 /*
  new UDT 4:
- with 2 member functions
  */
 
 /*
  new UDT 5:
- with 2 member functions
  */
 
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
-
  Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
  
  If you didn't already: 
     Make a pull request after you make your first commit
     pin the pull request link and this repl.it link to our DM thread in a single message.
-
  send me a DM to review your pull request when the project is ready for review.
-
  Wait for my code review.
  */
 
